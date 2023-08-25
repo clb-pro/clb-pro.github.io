5976d764df907119d16d03cc7bdb3d9c9c14ed9d
Zi Hanng_Ku <zihanng120@gmail.com>
2023-08-25 21:20:19 +0800

    last commit

--- portfolio/pokedex/pokedex.js
+++ portfolio/pokedex/pokedex.js
@@ -5,23 +5,54 @@ class PokemonService {
 
     constructor() {
         this.pokedex = new Pokedex.Pokedex();
+        this.offset = 0;
+        this.limit = 50;
+        this.totalPokemons = null;
+        this.pokemons = [];
     }
 
-    async getPokemonsList(offset = 0, limit = 20) {
+    async getPokemonsList() {
         try {
             var promises = []
-            const responseList = await this.pokedex.getPokemonsList({ offset, limit });
-                
+            // get pokemon name list
+            const responseList = await this.pokedex.getPokemonsList({ offset: this.offset, limit: this.limit });
+            this.totalPokemons = responseList.count;
+
+            // loop each pokemon to get its detail
             responseList.results.forEach(element => {
-                var promise = this.pokedex.getPokemonsByName(element.name)    
-                promises.push(promise)
+                var promise = this.pokedex.getPokemonByName(element.name)    
+                promises.push(promise);
             });
-            const responseDetails = await Promise.all(promises);
-            return responseDetails
             
+            const responseDetails = await Promise.all(promises);
+
+            // push the results to this.pokemons array
+            this.pokemons = [...this.pokemons, ...responseDetails]
+
+            return responseDetails;
         } catch (error) {
-            throw new Error("Error fetching Pokémon data");
+            console.error(error.message);
         }
+    }
+
+    isAllPokemonLoaded() {
+        if (this.pokemons.length === this.totalPokemons) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getTotalPokemons () {
+        return this.totalPokemons;
+    }
+
+    getTotalLoadedPokemons () {
+        return this.pokemons.length;
+    }
+    
+    next() { 
+        this.offset += this.limit; 
     }
 }
 
@@ -37,28 +68,49 @@ class HtmlBuilder {
     }
 
     createCard(pokemon) {
-         
+        let div = document.createElement("div");
+        div.innerHTML = pokemon.name;
+        this.container.append(div); 
     }
   
     displayPokemonCards(pokemons) {
-         console.log(pokemons)
+        this.container.scrollIntoView({ behavior: "smooth", block: "end" });
+        pokemons.forEach(pokemon => this.createCard(pokemon))
     }
 }
   
-  // Function to initiate the app and fetch/display Pokémon cards
+// initiate the app and fetch/display Pokémon cards
 async function startApp() {
     const pokemonService = new PokemonService();
     const htmlBuilder = new HtmlBuilder(document.getElementById('content'));
-
-    try {
-        // Fetch the list of Pokémon from the PokeAPI
+    const nextBtn = document.getElementById('next');
+    const loadedPokemonCount = document.getElementById('loaded-pokemons');
+    const totalPokemonCount = document.getElementById('total-pokemons');
+    
+    const loadPokemons = async () => {
         const pokemons = await pokemonService.getPokemonsList();
-
-        // Display the Pokémon cards
         htmlBuilder.displayPokemonCards(pokemons);
+
+        if (pokemonService.isAllPokemonLoaded()) {
+            nextBtn.disabled = true;
+        }
+        loadedPokemonCount.textContent = pokemonService.getTotalLoadedPokemons();
+        totalPokemonCount.textContent = pokemonService.getTotalPokemons();
+    };
+    
+    // initial load
+    try {
+        loadPokemons();
     } catch (error) {
         console.error(error.message);
     }
+
+    // implement the next button function
+    // or you can implement a "infinite scrolling" like how facebook load the news feed
+    nextBtn.addEventListener('click', async () => {
+        pokemonService.next();
+        loadPokemons();
+    })
 }
   
 // Start the app when the DOM is loaded
